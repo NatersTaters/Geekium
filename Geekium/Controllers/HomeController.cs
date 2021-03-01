55c25bc604b5ba74bd68d13e09a544d1f2159bde
Leunard Gervalla <Lgervalla8340@conestogac.on.ca>
2021-02-28 20:51:20 -0800

    Home Controller and Price Trends
    Home controller and views

--- Geekium/Controllers/HomeController.cs
+++ Geekium/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace Geekium.Controllers
 			return View();
 		}
 
+		public IActionResult Credit()
+        {
+			return View();
+        }
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
--- Geekium/wwwroot/js/site.js
+++ Geekium/wwwroot/js/site.js
@@ -7,3 +7,24 @@ $('.carousel').carousel({
     interval: 5000 //Speed of navigating the carousel images
 })
 
+// Filter the Search bar in the FAQ page
+
+function filterListing() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchFAQ');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("listFAQ");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through and filter list to what is typed in search bar
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
