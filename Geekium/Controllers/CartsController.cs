0b42a5df8dd5fd459d18aa18376e827d232cf7d7
Leunard Gervalla <Lgervalla8340@conestogac.on.ca>
2021-04-13 12:09:08 -0700

    Email notification, tracking and Cart

--- Geekium/Controllers/CartsController.cs
+++ Geekium/Controllers/CartsController.cs
@@ -11,13 +11,14 @@ using Geekium.Helpers;
 using Stripe;
 using System.Net.Mail;
 using System.Net;
+using System.Text;
 
 namespace Geekium.Controllers
 {
     public class CartsController : Controller
     {
         private readonly GeekiumContext _context;
-
+        private long stripePay;
         public CartsController(GeekiumContext context)
         {
             _context = context;
@@ -52,7 +53,15 @@ namespace Geekium.Controllers
 
                 // Calculate subtotal
                 var model = await cartItems.ToListAsync();
-                ViewBag.subTotal = SubTotal(model); // This is not updating properly
+                //ViewBag.subTotal = SubTotal(model); // This is not updating properly
+                int subTotal = (int)SubTotal(model);
+                HttpContext.Session.SetInt32("subTotal", subTotal);
+                stripePay = (long)SubTotal(model);
+
+                // Calculate points
+                //ViewBag.points = PointsEarned(ViewBag.subTotal);
+                string points = PointsEarned(subTotal).ToString();
+                HttpContext.Session.SetString("points", points);
 
                 return View(model);
             }
@@ -186,89 +195,108 @@ namespace Geekium.Controllers
         #endregion
 
         #region Checkout Functions
-        //Checkout - THIS IS WHERE PAYMENT PORTAL
-        //public async Task<IActionResult> CheckOut()
-        public async Task<IActionResult> CheckOut(string stripeEmail, string stripeToken, bool charged)
+        //Payment page
+        public IActionResult CartPayment()
         {
-            var customers = new CustomerService();
-            var charges = new ChargeService();
-
-            //int stripeAmount = Convert.ToInt32(ViewBag.stripeTotal * -1); // Test line
-            //var amount = (int)(stripeAmount); /// Test line
-
-            var customer = customers.Create(new CustomerCreateOptions{ 
-                Email=stripeEmail,
-                Source=stripeToken
-            });
-            var charge = charges.Create(new ChargeCreateOptions
-            {
-                /*TODO: amount being set as negative, charge isn't made because of it */
-                Amount = 500,//amount, //ViewBag.stripeTotal, //500, //ViewBag.total * 1000, //Convert.ToInt32(ViewBag.total * 1000),
-                Description = "Purchase off of Geekium",
-                Currency = "cad",
-                Customer = customer.Id,
-                ReceiptEmail = stripeEmail,
-                Metadata=new Dictionary<string, string>()
-                {
-                    {"OrderId","111" },
-                    {"OrderFrom", "Geekium" }
-                }
-            });
-
-            string accountId = HttpContext.Session.GetString("userId");
-
-            // Find the cart associated with this account
-            var cartContext = await _context.Cart
-                .Where(s => s.TransactionComplete == false)
-                .FirstOrDefaultAsync(s => s.AccountId.ToString() == accountId);
-
-            // Find all the items associated with this cart
-            var cartItems = _context.ItemsForCart
-                .Include(s => s.SellListing)
-                .Include(s => s.Cart)
-                .Where(s => s.SellListingId == s.SellListing.SellListingId)
-                .Where(s => s.CartId == cartContext.CartId).ToList();
-
-            if (charge.Status == "succeeded")
-            {
-                string BalanceTransactionId = charge.BalanceTransactionId;
-                customerNotifEmail(cartItems, charge);
-                //sellerNotifEmail(cartItems, charge);
-                return View("CheckOut");
-            }
-            else
-            {
-                return View("Unsuccessful");
-            }
+            return View();
         }
 
-        /*TODO: Customer email, layout as:
-         *-----------------------------------
-         *
-         *Subject: Purchase Confirmation
-         *Sender: geekium1234@gmail.com
-         *
-         *Body:  
-         *Thank you for your recent purchase at Geekium of products: 
-         * Purchase of: Geekium T-shirt - 19
-         * Purchase of: Item 2 - 20
-         * Purchase of: Item 3 - 35 
-         *For a total of: 74 
-         *Has been processed
-         *
-         */
-        
+        //Checkout - THIS IS WHERE PAYMENT PORTAL
+        //public async Task<IActionResult> CheckOut()
+            public async Task<IActionResult> CheckOut(string stripeEmail, string stripeToken, bool charged)
+            {
+                var customers = new CustomerService();
+                var charges = new ChargeService();
+
+                //int stripeAmount = Convert.ToInt32(ViewBag.subTotal * -1); // Test line
+                //var amount = (int)(stripeAmount); /// Test line
+
+                var customer = customers.Create(new CustomerCreateOptions{ 
+                    Email=stripeEmail,
+                    Source=stripeToken
+                });
+
+                //long amount = long.Parse(HttpContext.Session.GetInt32("subTotal"));
+                int a = (int)HttpContext.Session.GetInt32("subTotal");
+                long amount = Convert.ToInt64(a);
+                long sAmount = stripePay * 100;
+                //long.TryParse(HttpContext.Session.GetString("subTotal"), out amount);
+
+                var charge = charges.Create(new ChargeCreateOptions
+                {
+                    Amount = sAmount,
+                    Description = "Purchase off of Geekium",
+                    Currency = "cad",
+                    Customer = customer.Id,
+                    ReceiptEmail = stripeEmail,
+                    Metadata=new Dictionary<string, string>()
+                    {
+                        {"OrderId","111" },
+                        {"OrderFrom", "Geekium" }
+                    }
+                });
+
+                string accountId = HttpContext.Session.GetString("userId");
+
+                // Find the cart associated with this account
+                var cartContext = await _context.Cart
+                    .Where(s => s.TransactionComplete == false)
+                    .FirstOrDefaultAsync(s => s.AccountId.ToString() == accountId);
+
+                // Find all the items associated with this cart
+                var cartItems = _context.ItemsForCart
+                    .Include(s => s.SellListing)
+                    .Include(s => s.Cart)
+                    .Where(s => s.SellListingId == s.SellListing.SellListingId)
+                    .Where(s => s.CartId == cartContext.CartId).ToList();
+
+                if (charge.Status == "succeeded")
+                {
+                    string BalanceTransactionId = charge.BalanceTransactionId;
+                    customerNotifEmail(cartItems, charge);
+                    //sellerNotifEmail(cartItems, charge);
+                return View("CheckOut");
+                }
+                else
+                {
+                    return View("Unsuccessful");
+                }
+            }
+            /* Customer email, layout as:
+             *-----------------------------------
+             *
+             *Subject: Purchase Confirmation
+             *Sender: geekium1234@gmail.com
+             *
+             *Body:  
+             *Thank you for your recent purchase at Geekium: 
+             * Purchase of: Geekium T-shirt - $19
+             * Purchase of: Item 2 - $20
+             * Purchase of: Item 3 - $35 
+             *Total: $74 
+             *
+             *
+             */
+
         public void customerNotifEmail(List<ItemsForCart> cart, Charge charge)
-        {          
-            var pBody = "";
-            var price = "";
+        {
+            //Initialize new instance of stringbuilder
+            StringBuilder sb = new StringBuilder();
+
+            //Since Stripes pricing is 100 = $1, 1000 = $10, 10000 = $100, the charge.Amount is divided by 100 to get proper pricing
+            //So instead of the output being:
+            //Total: $500 
+            //It will display as 
+            //Total: $5
+            var amount = charge.Amount / 100;
+
+
+            var body = "Thank you for your recent purchase at Geekium: 0 \n" + "Total: $" + amount;
+            sb.Append(body);
             for (int i = 0; i < cart.Count; i++)
             {
-                pBody = "Purchase of: " + cart[i].SellListing.SellTitle;
-                price = " - {0}" + cart[i].SellListing.SellPrice;
+                sb.Insert(sb.ToString().IndexOf("0 "), " \n  Purchase of: " + cart[i].SellListing.SellTitle + " - $" + cart[i].SellListing.SellPrice);
             }
-
-            var body = "Thank you for your recent purchase at Geekium of products: \n" + pBody + price + "\n" + "For a total of: " + charge.Amount;
             var sEmail = new MailAddress("geekium1234@gmail.com");
             var rEmail = new MailAddress(HttpContext.Session.GetString("userEmail"));
             var password = "geekiumaccount1234";
@@ -285,19 +313,61 @@ namespace Geekium.Controllers
             using (var message = new MailMessage(sEmail, rEmail)
             {
                 Subject = sub,
-                Body = body
+                Body = sb.ToString()
             })
             {
                 smtp.Send(message);
             }
         }
-        public void sellerNotifEmail(List<ItemsForCart> cart, Charge charge, SellListing sellListing)
+             /* Seller email, layout as:
+             *-----------------------------------
+             *
+             *Subject: Product Sold
+             *Sender: geekium1234@gmail.com
+             *
+             *Body:  
+             *One of your products have recently been sold: 
+             * Product: Geekium T-shirt - 19
+             * Product: Item 2 - 20
+             * Product: Item 3 - 35 
+             *
+             *
+             */
+        public void sellerNotifEmail(List<ItemsForCart> cart, Charge charge)
         {
-            var pBody = "";
-            var price = "";
-            for  (int i = 0; i < cart.Count; i++)
-            {
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sbEmail = new StringBuilder();
 
+            var body = "One of your products have recently been sold: 0 \n";
+            var email = "Placeholder";
+
+            sb.Append(body);
+            sbEmail.Append(email);
+            for (int i = 0; i < cart.Count; i++)
+            {
+                sb.Insert(sb.ToString().IndexOf("0 "), " \n  Product: " + cart[i].SellListing.SellTitle + " - $" + cart[i].SellListing.SellPrice);
+                sbEmail.Insert(sb.ToString().IndexOf("Placeholder "), cart[i].SellListing.Seller.Account.Email);
+            }
+            var sEmail = new MailAddress("geekium1234@gmail.com");
+            var rEmail = new MailAddress(email); //TODO: Find out why it's returning null
+            var password = "geekiumaccount1234";
+            var sub = "Purchase Confirmation";
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(sEmail.Address, password)
+            };
+            using (var message = new MailMessage(sEmail, rEmail)
+            {
+                Subject = sub,
+                Body = sb.ToString()
+            })
+            {
+                smtp.Send(message);
             }
         }
         #endregion
@@ -328,6 +398,12 @@ namespace Geekium.Controllers
         public double TotalCost(double totalWithoutTax, double tax)
         {
             return totalWithoutTax + tax;
+        }
+
+        public double StripeAmount(double total)
+        {
+            double stripeRate = 100;
+            return total * stripeRate;
         }
 
         //Points are calculated by totalPoints(total cost of items) times the point rate of 10 and is rounded to the nearest whole number away from zero 
